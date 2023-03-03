4f5d6c2123f9179a686e13f22eabfb1f801cb17b
Kjetil Haugland <hauglandkjetil@gmail.com>
2023-03-03 20:15:59 +0100

    Updated posten.js with more urls
    Since posten.no for the second time in a few weeks broke the url by changing one numer I updated the script so it will attempt to fetch with the number 0-50 to automatically catch future breaks.

--- posten.js
+++ posten.js
@@ -1,27 +1,33 @@
-const postnummer = 0000;
+const postnummer = 0000
 
-const apiurl =
-  "https://www.posten.no/levering-av-post/_/component/main/1/leftRegion/9?postCode=" +
-  postnummer;
-
-async function getData() {
-  const resp = await fetch(apiurl, {
-    method: "get",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Requested-With": "XMLHttpRequest",
-    },
-  });
-
-  return resp.json();
+async function getData(code) {
+  const apiurl = `https://www.posten.no/levering-av-post/_/component/main/1/leftRegion/${code}?postCode=${postnummer}`;
+  console.log(apiurl);
+    const resp = await fetch(apiurl, {
+      method: 'get',
+      headers: {
+        "Content-Type": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    });
+    return resp.json();
 }
 
-const response = await getData();
+// Got sick of Posten.no changing the one number in the url and breaking it, so trying until success between 0-50.
+for(let i = 0; i < 50; i++) {
+  try {
+    response = await getData(i);
+    break;
+  } catch(e) {
+    response = { nextDeliveryDays: [] }
+  }  
+}
+
 function isPostToday() {
-  if (response.nextDeliveryDays && response.nextDeliveryDays[0]) {
-    if (response.nextDeliveryDays[0].includes("i dag")) {
+  if(response.nextDeliveryDays && response.nextDeliveryDays[0]) {
+    if(response.nextDeliveryDays[0].includes('i dag')) {
       return true;
-    }
+    } 
   }
   return false;
 }
